959a7fb8531090352b2708c101ed99926b08dcfd
Andres <andres.correa@designli.co>
2025-02-03 16:13:53 -0500

    fix: url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,8 +10,7 @@ const config: Config = {
 	favicon: "img/favicon.ico",
 
 	// Set the production url of your site here
-	url: "https://cruiz-designli.github.io",
-	// url: "https://cruiz-designli.github.io/MEO-documentation",
+	url: "https://cruiz-designli.github.io/MEO-documentation",
 	// Set the /<baseUrl>/ pathname under which your site is served
 	// For GitHub pages deployment, it is often '/<projectName>/'
 	baseUrl: "/",

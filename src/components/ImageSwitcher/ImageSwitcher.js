df45f1a4ad445ec0067e07b04db7130df2aea539
Andres <andres.correa@designli.co>
2025-02-03 16:29:44 -0500

    fix: images

--- src/components/ImageSwitcher/ImageSwitcher.js
+++ src/components/ImageSwitcher/ImageSwitcher.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useColorMode } from '@docusaurus/theme-common'; //docs: https://v2.docusaurus.io/docs/2.0.0-alpha.69/theme-classic#usethemecontext
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 const ImageSwitcher = ({ lightImageSrc, darkImageSrc }) => {
 	const { colorMode, setColorMode } = useColorMode();
